6c4a1d902a5f4e4874527d49d9ea03f93cdab1c0
GranSpencer09 <Granspencer09@gmail.com>
2022-08-20 13:03:36 -0700

    Major changes to HTML, JS, & README files. Adding screenshot of the app, while it's not totally complete yet.

--- README.md
+++ README.md
@@ -1,79 +1,25 @@
 ## A-Bit-Weathery
 
+https://github.com/GranSpencer09/a-bit-weathery
+https://granspencer09.github.io/a-bit-weathery/
+
 ## Description
 
-Provide a short description explaining the what, why, and how of your project.
-Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a
-  homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to
-find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+This website helps anyone who's wondering what the weather will be like in any city around the world. If they need forecast before visiting a city, or if they'd like to know what the current weather is they can use A-Bit-Weathery. This project also stores the cities a user searches for so they can easily access their weather data again. To build all of this, we of course need to pull data from the OpenWeather and OpenUV databases. We also used local storage to retain previous queries. During this project, I learned how to organize more complicated JS logic and construct a HTML outline that makes DOM manipulation easier. I created the outline in HTML and then removed those elements as I coded them in to appear with JS after a user's action.
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step
-description of how to get the development environment running.
+- Go to the code repo
+- Located near the about section, find and press the green code button
+- Copy either the HTTPS, Git CLI, download the zip, open with GitHub desktop, or copy the SSH link.
+- Depending on download method, use Git or the desktop application to open the content files
+- All of the content of the repository will be available after completion of the previous state.
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-To add a screenshot, create an `assets/images` folder in your repository and
-upload your screenshot to it. Then, using the relative file path, add it to
-your README using the following syntax:
-`md ![alt text](assets/images/screenshot.png) `
+`md ![What's displayed after a user enters a city](./screen-shot) `
+To use this, open the app and enter in a city under in the search bar and click 'search'. The user will immediately see the results. The current weather and the name of the city searched for appear at the top of the screen. Beneath this is the 5 day forecast for the city the user searched for. Finally, after the user searches for a city, a button with the name of that city appears beneath the search bar. If the user wants to check the weather in a city they've already searched for, they can click the button that's appeared to do so.
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-If you used any third-party assets that require attribution, list the creators
-with links to their primary web presence in this section.
-If you followed tutorials, include links to those here as well.
-
-## License
-
-The last section of a high-quality README file is the license. This lets other
-developers know what they can and cannot do with your project. If you need
-help choosing a license, refer to
-
-[https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let
-other developers know that you know what you're doing. Check out the badges
-hosted by [shields.io](https://shields.io/). You may not understand what they
-all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to
-contribute it, you can include guidelines for how to do so. The [Contributor
-Covenant](https://www.contributor-covenant.org/) is an industry standard, but
-you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples
-on how to run them here.
+The OpenWeather and OpenUV APIs were of course helpful. My tutor was also very helpful.
--- script.js
+++ script.js
@@ -1,45 +1,203 @@
-var searchEl = document.getElementById("search");
+//searchBtn.onchange = search();
+// when user clicks the search button, run search function
+// search () {
+//// grab the text the user added to searchEl and add it to variable
+//// fully lowercase the text
+//// add the text to the fetch query
+//// from the query return the following in these places
+//// return the name of city in cityCurrent
+//// return the weather symbol of city in citySymbol
+//// store name and lat/long of city in local storage
+////// for loop (to create new place to store city and lat/long name in LS)
+////// in same for loop, create new button
+////// text content name of city to new button
+////// store city lat and long in variables
+//////// pass lat and long to the UV fetch
+//////// access the UV Index
+//////// return the temp, wind, humidity in currentTWHUV
+//////// return the current UV
+////////// add green, yellow or ren to indicate whether the conditions are
+//           favorable, moderate, or severe
+//////// return the 5 day forcast for the city
+///////// each box needs date, temp wind & humidity
+// if user clicks one of the 'saved city buttons',
+// ..display city data again by grabbing the lat and long of the city
 
-fetch(
-  "http://api.openweathermap.org/geo/1.0/direct?q=san francisco&limit=5&appid=b76aeff8b2ed6817e2d4af8ad0304159"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[i].country);
-      //searchEl.textContent = data[i].country;
-    }
-  });
+// ref search button
+var searchBtn = document.getElementById("searchBtn");
+// ref for search box
+var tagsSearch = document.getElementById("tags-search");
 
-$(function () {
-  var availableTags = [
-    "ActionScript",
-    "AppleScript",
-    "Asp",
-    "BASIC",
-    "C",
-    "C++",
-    "Clojure",
-    "COBOL",
-    "ColdFusion",
-    "Erlang",
-    "Fortran",
-    "Groovy",
-    "Haskell",
-    "Java",
-    "JavaScript",
-    "Lisp",
-    "Perl",
-    "PHP",
-    "Python",
-    "Ruby",
-    "Scala",
-    "Scheme",
-  ];
-  $("#tags").autocomplete({
-    source: availableTags,
-  });
+// ref large current city h2
+var cityCurrent = document.getElementById("cityCurrent");
+// ref large current date h2
+var currentDateSymbol = document.getElementById("currentDateSymbol");
+
+var city5Fc = document.getElementById("city5Fc");
+
+var cityButtons = document.getElementById("cityButtons");
+
+var currentTemp = document.getElementById("currentTemp");
+var currentWind = document.getElementById("currentWind");
+var currentHum = document.getElementById("currentHum");
+var currentUV = document.getElementById("currentUV");
+
+var btn = document.getElementById("btn");
+
+// creating places to store city in LS
+var cityStored = localStorage.getItem(cityStored);
+// store the latitude
+var latStored = localStorage.getItem(latStored);
+// store the longitude
+var lngStored = localStorage.getItem(lngStored);
+
+// setting the limit to 5, given free account limits
+var limit = 5;
+
+var lat = "";
+var lng = "";
+
+// setting the limit to 5, given free account limits
+var cities = [];
+
+// listener to click on the search button
+searchBtn.addEventListener("click", function () {
+  if (tagsSearch.value === "") {
+    // this alert won't show up even if the user doesn't enter anything
+    alert("You must enter a city!");
+    return;
+  } else {
+    storeInput();
+  }
 });
+
+function storeInput() {
+  // store user input in variable
+  var cityEntered = tagsSearch.value.trim();
+
+  cities.push(cityEntered);
+  tagsSearch.value = "";
+  city5Fc.value = "";
+  // Use .setItem() to store object in storage and JSON.str
+  // ingify to convert it as a string
+  localStorage.setItem("cityStored", JSON.stringify(cityEntered));
+
+  console.log();
+
+  var btn = document.createElement("button");
+  btn.classList.add("btn", "btn-secondary", "mt-md-2");
+  btn.setAttribute("id", "btn");
+
+  btn.textContent = cityEntered;
+
+  cityButtons.appendChild(btn);
+
+  // variables for first API
+  const key1 = "b76aeff8b2ed6817e2d4af8ad0304159";
+  const baseUrl1 = "https://api.openweathermap.org/geo/1.0/direct?q=";
+  const query1 = `${cityEntered}&limit=${limit}&appid=${key1}`;
+
+  $.ajax({
+    url: baseUrl1 + query1,
+    method: "GET",
+  }).then((value) => {
+    lat = value[0].lat;
+    lng = value[0].lon;
+
+    cities.push(lat);
+    cities.push(lng);
+
+    localStorage.setItem("lat", JSON.stringify(lat));
+    localStorage.setItem("lng", JSON.stringify(lng));
+
+    part2();
+  });
+}
+
+function part2() {
+  const key1 = "b76aeff8b2ed6817e2d4af8ad0304159";
+  const baseURL2 = "https://api.openweathermap.org/data/2.5/weather?";
+  const query2 = `lat=${lat}&lon=${lng}&appid=${key1}&units=imperial`;
+
+  $.ajax({
+    url: baseURL2 + query2,
+    method: "GET",
+  }).then((value) => {
+    part3();
+  });
+}
+
+function part3() {
+  const key1 = "b76aeff8b2ed6817e2d4af8ad0304159";
+  const baseURL3 = "https://api.openweathermap.org/data/2.5/forecast?";
+  const query3 = `lat=${lat}&lon=${lng}&appid=${key1}&units=imperial`;
+
+  $.ajax({
+    url: baseURL3 + query3,
+    method: "GET",
+  }).then((value) => {
+    for (let i = 4; i < value.list.length; i += 8) {
+      var divCard = document.createElement("div");
+      divCard.classList.add("col-md-2", "card", "border-light");
+      divCard.style.maxWidth = "18rem";
+      divCard.innerHTML = `<div class="card-body">
+                <h5 class="card-title">${value.list[i].dt_txt}</h5>
+                <p class="card-text">
+                <img src="http://openweathermap.org/img/wn/${value.list[i].weather[0].icon}@2x.png"/>
+                  <ul>
+                    <dd>Temp: ${value.list[i].main.temp}°</dd>
+                    <dd>Wind: ${value.list[i].wind.speed}mph</dd>
+                    <dd>Humidity: ${value.list[i].main.humidity}%</dd>
+                  </ul>
+                </p>
+              </div>`;
+      city5Fc.append(divCard);
+    }
+    //console.log(value);
+    cityCurrent.textContent = value.city.name;
+    //cityCurrentDateSymbol.textContent = date();
+
+    currentTemp.textContent = value.list[0].main.temp;
+    currentWind.textContent = value.list[0].wind.speed;
+    currentHum.textContent = value.list[0].main.humidity;
+
+    part4();
+  });
+}
+
+function part4() {
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    beforeSend: function (request) {
+      request.setRequestHeader(
+        "x-access-token",
+        "988c321c1ddde559e602bae0133f5498"
+      );
+    },
+    url:
+      "https://api.openuv.io/api/v1/uv?lat=" +
+      lat +
+      "&lng=" +
+      lng +
+      "&alt=" +
+      0 +
+      "&ozone=" +
+      0 +
+      "&dt=" +
+      0,
+    success: function (response) {
+      //for (var i = 0; i<)
+      //currentHum.textContent = response.uv;
+    },
+    error: function (response) {
+      // handle error response
+    },
+  });
+}
+
+btn.addEventListener("click", function () {
+  alert("ckick");
+});
+
+loadCities();
